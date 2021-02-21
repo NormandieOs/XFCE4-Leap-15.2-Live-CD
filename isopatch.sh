3b958d9a5b7029dc1883b0e3d24fd95df1c0b7fb
Norma76 <gilles.laffite76@gmail.com>
2021-02-21 18:24:17 +0100

    Update isopatch.sh

--- isopatch.sh
+++ isopatch.sh
@@ -9,6 +9,6 @@ grubcfg=${iso_dir}/boot/grub2/grub.cfg
 efi_grubcfg=${iso_dir}/EFI/BOOT/grub.cfg
 
 # default language
-gfxboot --default-language ru_RU -a ${iso_bootlogo}
-sed -i '0,/\$linux/s/\(quiet\)/\1 lang=ru_RU/' ${grubcfg}
-sed -i '0,/\$linux/s/\(quiet\)/\1 lang=ru_RU/' ${efi_grubcfg}
+gfxboot --default-language fr_FR -a ${iso_bootlogo}
+sed -i '0,/\$linux/s/\(quiet\)/\1 lang=fr_FR/' ${grubcfg}
+sed -i '0,/\$linux/s/\(quiet\)/\1 lang=fr_FR/' ${efi_grubcfg}
